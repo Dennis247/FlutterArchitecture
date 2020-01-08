54a734246c9b18a9cce686449ef3581007bf3897
Bolaji Bello <moborlergie1905@gmail.com>
2020-01-08 17:09:12 +0100

    initial commit

---
+++ lib/features/number_trivia/domain/usecases/get_concrete_trivia.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_arch/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+class GetConcreteNumberTrivia {
+  final NumberTriviaRepository numbertriviarepository;
+
+  GetConcreteNumberTrivia(this.numbertriviarepository);
+}
